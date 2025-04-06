0ee08ea466c55aa8ae144ea187ea76fe138b0f9e
Moksh Malde <maldemoksh437@gmail.com>
2025-04-06 23:42:25 +0530

    feat: add logout route to user routes and clean up imports

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/userController.js";
+import { register, login, logout } from "../controllers/userController.js";
 import {getProfile} from "../controllers/profileControllers.js"
 import {protect} from "../middleware/authMiddleware.js";
 const router = express.Router();
-
+router.get("/logout", logout);
+router.get("/me", protect, getProfile);
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", protect, getProfile);
 export default router;
