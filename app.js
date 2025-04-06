6c6f2fcf4e638c0de0004cd2df4c2c4b59780f21
Moksh Malde <maldemoksh437@gmail.com>
2025-04-06 09:58:55 +0530

    feat: configure CORS options and enable URL-encoded data parsing

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ import connectDB from "./config/db.js";
 dotenv.config();
 connectDB();
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 
 app.use("/api/auth", authRoutes);
